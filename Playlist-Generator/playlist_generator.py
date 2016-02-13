666db10a09b7b6fcd3819ea4a779035552d8b597
tmas <tmas@tgaragemedia.com>
2016-02-13 12:04:59 -0500

    yay sql >:(

--- Playlist-Generator/playlist_generator.py
+++ Playlist-Generator/playlist_generator.py
@@ -6,7 +6,7 @@ length = 0.0
 
 db = pymysql.connect(host="localhost", user="flur", password="KirklandSignature", db="flur", charset="utf8mb4", cursorclass=pymysql.cursors.DictCursor)
 
-sql = "SELECT * FROM flur WHERE 'genre' CONTAINS %s" % (genre)
+sql = "SELECT * FROM 'song' WHERE INSTR('genre', '%s')" % (genre)
 
 cursor = db.cursor()
 
