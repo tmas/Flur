4faa226397c9d3da96908cca5026ad71c02ec037
tmas <tmas@tgaragemedia.com>
2016-02-13 11:41:23 -0500

    testing

--- genre-query/genq.py
+++ genre-query/genq.py
@@ -17,6 +17,6 @@ class Song(Model):
     class Meta:
         database = db
 #Song.create_table(True);
-for track in Song.select().where(Song.genres.contains("rock")).get():
-    print(track.name, " is a rock song! Genres: ", track.genres)
-#print(Song.select().where(Song.genres.contains("rock")))
+#for track in Song.select().where(Song.genres.contains("rock")).get():
+#    print(track.name, " is a rock song! Genres: ", track.genres)
+print(Song.select().get())
