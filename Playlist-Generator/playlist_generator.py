3ca4813dfa9f0d0a0f98531e805a871d2dd482dd
tmas <tmas@tgaragemedia.com>
2016-02-13 12:12:52 -0500

    hardcoded database queries ft.

--- Playlist-Generator/playlist_generator.py
+++ Playlist-Generator/playlist_generator.py
@@ -6,13 +6,22 @@ length = 0.0
 
 db = pymysql.connect(host="localhost", user="flur", password="KirklandSignature", db="flur", charset="utf8mb4", cursorclass=pymysql.cursors.DictCursor)
 
-sql = "SELECT * FROM song"
+sql = "SELECT * FROM song WHERE INSTR(genres, 'rock')"
 
 cursor = db.cursor()
 
 cursor.execute(sql)
 
-data = cursor.fetchall()
+data = cursor.fetchone()
+
+print(data)
+data = cursor.fetchone()
+
+print(data)
+data = cursor.fetchone()
+
+print(data)
+data = cursor.fetchone()
 
 print(data)
 
