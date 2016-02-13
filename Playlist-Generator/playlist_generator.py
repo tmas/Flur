0a3b35a6c82efedac5a8e114fb9f5da0feef1937
tmas <tmas@tgaragemedia.com>
2016-02-13 12:31:43 -0500

    implemented most of playlist generator

--- Playlist-Generator/playlist_generator.py
+++ Playlist-Generator/playlist_generator.py
@@ -1,28 +1,35 @@
 import pymysql.cursors
 from peewee import *
+import sys
+import math
 #initialize data from front end
-genre = ""
-length = 0.0
+genre = "rock"
+desired_length = 1.0
+length = 0
+playlist = []
+if(len(sys.argv)==2):
+    genre=sys.argv[1]
+elif(len(sys.argv)==3):
+    genre=sys.argv[1]
+    desired_length=float(sys.argv[2])
+else:
+    print("Invalid number of arguments. Please try again.")
 
+desired_length = int(desired_length * 3600000);
 db = pymysql.connect(host="localhost", user="flur", password="KirklandSignature", db="flur", charset="utf8mb4", cursorclass=pymysql.cursors.DictCursor)
 
-sql = "SELECT * FROM song WHERE INSTR(genres, 'rock')"
+sql = "SELECT * FROM song WHERE INSTR(genres, '%s')"
 
 cursor = db.cursor()
 
-cursor.execute(sql)
+cursor.execute(sql, genre)
 
-data = cursor.fetchone()
+data = cursor.fetchall()
 
-print(data)
-data = cursor.fetchone()
-
-print(data)
-data = cursor.fetchone()
-
-print(data)
-data = cursor.fetchone()
-
-print(data)
-
+while (length < desired_length):
+    rnd = random.randint(0, len(data))
+    if not data[rnd]['url'] in playlist:
+        playlist.append(data[rnd]['url'])
+        length += data[rnd]['duration']
+print(playlist);
 db.close()
