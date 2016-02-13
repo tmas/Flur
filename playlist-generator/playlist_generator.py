387dd6bdb5c5d305aa2dac73f2133582cae42b11
tmas <tmas@tgaragemedia.com>
2016-02-13 13:04:57 -0500

    Adding new version of playlist-generator

--- playlist-generator/playlist_generator.py
+++ playlist-generator/playlist_generator.py
@@ -1,8 +1,36 @@
 import pymysql.cursors
 from peewee import *
-import MySQLdb
+import sys
+import random
 #initialize data from front end
-genre = ""
-length = 0.0
+genre = "rock"
+desired_length = 1.0
+length = 0
+playlist = []
+if(len(sys.argv)==2):
+    genre=sys.argv[1]
+elif(len(sys.argv)==3):
+    genre=sys.argv[1]
+    desired_length=float(sys.argv[2])
+else:
+    print("Invalid number of arguments. Please try again.")
 
-sql = "SELECT * FROM  WHERE 'genre'= %s" % \ (genre)
+desired_length = int(desired_length * 3600000);
+db = pymysql.connect(host="localhost", user="flur", password="KirklandSignature", db="flur", charset="utf8mb4", cursorclass=pymysql.cursors.DictCursor)
+
+sql = "SELECT * FROM song WHERE INSTR(genres, %s) AND popularity >= 50"
+
+cursor = db.cursor()
+
+cursor.execute(sql, genre)
+
+data = cursor.fetchall()
+
+while (length < desired_length):
+    rnd = random.randint(0, len(data))
+    if not data[rnd]['url'] in playlist:
+        playlist.append(data[rnd]['url'])
+        length += data[rnd]['duration']
+print("Songs: ", playlist);
+print("Duration: ", float(length)/3600000)
+db.close()
