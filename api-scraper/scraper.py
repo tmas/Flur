6409fd51bf0b424712bf9c0aa5f85716db8f42e1
tmas <tmas@tgaragemedia.com>
2016-02-13 04:25:08 -0500

    Completed scraping element of scraper

--- api-scraper/scraper.py
+++ api-scraper/scraper.py
@@ -1,22 +1,24 @@
 import spotipy
 spotify = spotipy.Spotify()
-results = spotify.search(q='track:' + '', limit=50, offset=0, type='track')
+results = spotify.search(q='track:' + '', limit=20, offset=0, type='track')
 tracks = results['tracks']
 items = tracks['items']
 i = 0
-songs = {}
-artistGenres = {'DummyID': 'DummyGenre'}
+songs = []
+artistGenres = {}
 for track in items:
     artists = ""
     for artist in track['artists']:
         artists += artist['name'] + " && "
     artists = artists.rstrip(" && ")
-    if (!artistGenres.has_key(track['artists'][0]['id'])):
+    if (not artistGenres.has_key(track['artists'][0]['id'])):
         artistResult = spotify.artist(track['artists'][0]['uri'])
         artistGenres[track['artists'][0]['id']] = ""
         for genre in artistResult['genres']:
             artistGenres[track['artists'][0]['id']] += genre + ", "
         artistGenres[track['artists'][0]['id']] = artistGenres[track['artists'][0]['id']].rstrip(", ")
-    songs[i] = {track['name'], artists, track['album']['name'], track['popularity'], track['duration_ms'], artistGenres[track['artists'][0]['id']], track['external_urls']['spotify']}
-    i += 1
-print songs[0]
+    songs.append({'name': track['name'], 'artists': artists, 'album': track['album']['name'], 'popularity': track['popularity'], 'duration': track['duration_ms'], 'genres': artistGenres[track['artists'][0]['id']], 'url': track['external_urls']['spotify']})
+for song in songs:
+    print song['name']
+    print song['genres']
+    print "\n"
