210a53116aeec7ce61272494af82eda85d074c4b
tmas <tmas@tgaragemedia.com>
2016-02-13 05:35:25 -0500

    Attempt to add mysql support

--- api-scraper/scraper.py
+++ api-scraper/scraper.py
@@ -1,11 +1,26 @@
 import spotipy
+from peewee import *
+import pymysql.cursors
 spotify = spotipy.Spotify()
-results = spotify.search(q='track:' + '', limit=20, offset=0, type='track')
-tracks = results['tracks']
-items = tracks['items']
-i = 0
 songs = []
 artistGenres = {}
+
+db = pymysql.connect(host="162.243.5.44",user="root",password="M1ddl30ut!",db="flur",charset="utf8mb4_bin",cursorclass=pymysql.cursors.DictCursor)
+class Song(Model):
+    name=CharField()
+    artists=CharField()
+    album=CharField()
+    popularity=SmallIntegerField()
+    duration=BigIntegerField()
+    genres=CharField()
+    url=CharField()
+
+    class Meta:
+        database = db
+
+results = spotify.search(q='track:' + '', limit=1, offset=0, type='track')
+tracks = results['tracks']
+items = tracks['items']
 for track in items:
     artists = ""
     for artist in track['artists']:
@@ -19,6 +34,6 @@ for track in items:
         artistGenres[track['artists'][0]['id']] = artistGenres[track['artists'][0]['id']].rstrip(", ")
     songs.append({'name': track['name'], 'artists': artists, 'album': track['album']['name'], 'popularity': track['popularity'], 'duration': track['duration_ms'], 'genres': artistGenres[track['artists'][0]['id']], 'url': track['external_urls']['spotify']})
 for song in songs:
-    print song['name']
-    print song['genres']
-    print "\n"
+    print(song['name'])
+    print(song['genres'])
+    print("\n")
