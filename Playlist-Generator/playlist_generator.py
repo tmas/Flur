7ca35dd430885aa4e88d1ad826757ae2d51b951b
Joseph Rogers <jdr86@pitt.edu>
2016-02-13 11:04:03 -0500

    Added test file that hopefully will access the server

---
+++ Playlist-Generator/playlist_generator.py
@@ -0,0 +1,18 @@
+import pymysql.cursors
+from peewee import *
+import MySQLdb
+#initialize data from front end
+genre = ""
+length = 0.0
+
+db = MySQLdb.connect("localhost", "testuser", "test123", "flur")
+
+sql = "SELECT * FROM  WHERE 'genre'= %s" % \ (genre)
+
+cursor = db.cursor()
+
+cursor.execute(sql)
+
+data = cursor.fetchone()
+
+print(data)
