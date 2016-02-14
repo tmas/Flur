64c5d42733362612bfb5ec127c67e2d68e71646b
tmas <tmas@tgaragemedia.com>
2016-02-14 07:15:53 -0500

    LINKIN SQL

--- flur/flur.py
+++ flur/flur.py
@@ -19,49 +19,76 @@ def safePlaytime(artstplytm, artist):
 
 	#returns ids of all the songs
 def getPlaylist(duration, g, pop_low, pop_up, ng, solubility=0.4, linkin):
-	genre = g
-	desired_length = duration
-	notgenre = ng
-	if not notgenre == "":
-		notgenre = "%"+ng+"%"
-	length = 0
-	playlist = []
-	artistplaytime = {}
-	desired_length = int(float(desired_length) * 3600000);
-	db = pymysql.connect(host="localhost", user="flur", password="KirklandSignature", db="flur", charset="utf8mb4", cursorclass=pymysql.cursors.DictCursor)
-	s = Template("SELECT * FROM song WHERE INSTR(genres, '$genre') AND popularity >= '$pop_low' AND popularity <= '$pop_up' AND genres NOT LIKE '$notgenre' ORDER BY RAND()")
-	sql = s.substitute(genre=genre, pop_low=pop_low, pop_up=pop_up, notgenre=notgenre)
-	print(sql)
+	if not linkin:
+		genre = g
+		desired_length = duration
+		notgenre = ng
+		if not notgenre == "":
+			notgenre = "%"+ng+"%"
+		length = 0
+		playlist = []
+		artistplaytime = {}
+		desired_length = int(float(desired_length) * 3600000);
+		db = pymysql.connect(host="localhost", user="flur", password="KirklandSignature", db="flur", charset="utf8mb4", cursorclass=pymysql.cursors.DictCursor)
+		s = Template("SELECT * FROM song WHERE INSTR(genres, '$genre') AND popularity >= '$pop_low' AND popularity <= '$pop_up' AND genres NOT LIKE '$notgenre' ORDER BY RAND()")
+		sql = s.substitute(genre=genre, pop_low=pop_low, pop_up=pop_up, notgenre=notgenre)
+		print(sql)
 
-	cursor = db.cursor()
+		cursor = db.cursor()
 
-	cursor.execute(sql)
+		cursor.execute(sql)
 
-	data = cursor.fetchall()
-	print("got data")
-	songsadded=0
-	#while (length < desired_length):
-	#	if (songsadded >= len(data)):
-	#		break
-	#	rnd = random.randint(0, len(data)-1)
-	#	if not data[rnd]['url'] in playlist:
-	#		playlist.append(data[rnd]['url'])
-	#		#print(data[rnd]['name'])
-	#		#identification = url[31:]
-	#		#ids.append(identification)
-	#		length += data[rnd]['duration']
-	#		songsadded += 1
-	for song in data:
-		if length >= desired_length:
-			break
-		if (not song['url'] in playlist) and ((float(safePlaytime(artistplaytime, song['artists']) + song['duration']) / float(desired_length)) < solubility):
-			playlist.append(song['url'])
-			length += song['duration']
-			artistplaytime[song['artists']] = safePlaytime(artistplaytime, song['artists']) + song['duration']
-	print("Songs: ", playlist);
-	print("Duration: ", float(length)/3600000)
-	db.close()
-	return playlist
+		data = cursor.fetchall()
+		print("got data")
+		songsadded=0
+		#while (length < desired_length):
+		#	if (songsadded >= len(data)):
+		#		break
+		#	rnd = random.randint(0, len(data)-1)
+		#	if not data[rnd]['url'] in playlist:
+		#		playlist.append(data[rnd]['url'])
+		#		#print(data[rnd]['name'])
+		#		#identification = url[31:]
+		#		#ids.append(identification)
+		#		length += data[rnd]['duration']
+		#		songsadded += 1
+		for song in data:
+			if length >= desired_length:
+				break
+			if (not song['url'] in playlist) and ((float(safePlaytime(artistplaytime, song['artists']) + song['duration']) / float(desired_length)) < solubility):
+				playlist.append(song['url'])
+				length += song['duration']
+				artistplaytime[song['artists']] = safePlaytime(artistplaytime, song['artists']) + song['duration']
+		print("Songs: ", playlist);
+		print("Duration: ", float(length)/3600000)
+		db.close()
+		return playlist
+	else:
+		desired_length = duration
+		length = 0
+		playlist = []
+		desired_length = int(float(desired_length) * 3600000);
+		db = pymysql.connect(host="localhost", user="flur", password="KirklandSignature", db="flur", charset="utf8mb4", cursorclass=pymysql.cursors.DictCursor)
+		sql = "SELECT * FROM `song` WHERE `artists` LIKE 'Linkin Park' ORDER BY RAND()"
+		print(sql)
+
+		cursor = db.cursor()
+
+		cursor.execute(sql)
+
+		data = cursor.fetchall()
+		print("got data")
+		songsadded=0
+		for song in data:
+			if length >= desired_length:
+				break
+			if (not song['url'] in playlist):
+				playlist.append(song['url'])
+				length += song['duration']
+		print("Songs: ", playlist);
+		print("Duration: ", float(length)/3600000)
+		db.close()
+		return playlist
 
 @flur.route('/')
 def index():
