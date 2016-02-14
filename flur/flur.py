21713e5f250230cb1c814c35b1bf8b3b4bd01aa1
tmas <tmas@tgaragemedia.com>
2016-02-14 05:53:25 -0500

    :|

--- flur/flur.py
+++ flur/flur.py
@@ -54,9 +54,10 @@ def getPlaylist(duration, g, pop_low, pop_up, ng, solubility=0.4):
 	for song in data:
 		if length >= desired_length:
 			break
-		if not song['url'] in playlist: #and ((float(safePlaytime(artistplaytime, song['artists']) + song['duration']) / float(duration)) < solubility):
+		if (not song['url'] in playlist) and ((float(safePlaytime(artistplaytime, song['artists']) + song['duration']) / float(duration)) < solubility):
 			playlist.append(song['url'])
 			length += song['duration']
+			artistplaytime[song['artists']] += song['duration']
 	print("Songs: ", playlist);
 	print("Duration: ", float(length)/3600000)
 	db.close()
